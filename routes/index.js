07764f7a4204f647db5282dcbd3639eb68131296
pissoarsniffare <samuel.viglundsson@elev.ga.ntig.se>
2024-03-12 10:35:04 +0100

    emil

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ router.get('/', function (req, res) {
   res.render('index.njk', { title: 'Welcome' })
 })
 
+router.get('/login', function (req, res) {
+    res.render('login.njk', { title: 'Welcome' })
+})
+
+router.get('/signup', function (req, res) {
+    res.render('signup.njk', { title: 'Welcome' })
+})
+
 module.exports = router
