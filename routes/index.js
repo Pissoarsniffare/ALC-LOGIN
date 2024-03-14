6a4b7cf5755efb0afce91b9f4be460b963b2da98
pissoarsniffare <samuel.viglundsson@elev.ga.ntig.se>
2024-03-14 11:20:42 +0100

    gjorde lösenord grejs

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,12 @@ const router = express.Router()
 
 const pool = require('../db')
 
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const myPlaintextPassword = 'test';
+const someOtherPlaintextPassword = 'not_bacon';
+
+
 router.get('/', function (req, res) {
   res.render('index.njk', { title: 'Welcome' })
 })
@@ -18,6 +24,16 @@ router.get('/signup', function (req, res) {
 router.get('/dbtest', async function (req, res) {
     const [result] = await pool.promise().query('SELECT * FROM SÅSIALDMÅKRATERN_LOGIN')
     res.json({result})
+})
+
+router.get('/hashtest', async function (req, res){
+
+    bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        console.log(hash)
+    });
+
+    res.send("nuggets")
 })
 
 module.exports = router
